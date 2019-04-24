681a28b88ab64191aea8d7851787553b1aa07bc0
Josh Holtkort <jholtkort@utexas.edu>
2019-04-24 16:27:04 -0500

    Add GH Pages

--- src/components/apis/openweather.js
+++ src/components/apis/openweather.js
@@ -2,8 +2,16 @@ import axios from "axios";
 
 const KEY = "11b5fe4b97c7ea5b11013a5a7f4fbcb3";
 
+// let baseURL = "";
+
+// if (location.protocol === "http:") {
+//   baseURL = "http://api.openweathermap.org";
+// } else {
+//   baseURL = "https://api.openweathermap.org";
+// }
+
 export default axios.create({
-  baseURL: "http://api.openweathermap.org",
+  baseURL: "https://api.openweathermap.org",
   params: {
     appid: KEY
   }
